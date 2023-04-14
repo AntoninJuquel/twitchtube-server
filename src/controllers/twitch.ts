96a82a0e07237674fd4c76e1c82c61772a0ecc65
AntoninJuquel <antonin.juquel@outlook.fr>
2023-04-14 06:51:02 +0200

    better errors

--- src/controllers/twitch.ts
+++ src/controllers/twitch.ts
@@ -6,6 +6,17 @@ export async function getClips(req: Request, res: Response) {
   try {
     res.status(200).json(await twitch.getClips(req.query));
   } catch (err) {
-    res.status(500).json({ message: (err as Error).message });
+    const error = err as Error;
+    if (["ValidationError"].includes(error.name)) {
+      res.status(400).json({ message: error.message });
+    } else if (
+      ["GameNotFound", "NoClipsFound", "UserNotFound", "InvalidType"].includes(
+        error.name
+      )
+    ) {
+      res.status(404).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: error.message });
+    }
   }
 }
--- src/modules/twitch.ts
+++ src/modules/twitch.ts
@@ -1,4 +1,4 @@
-import Twitch from "twitch-api-helix";
+import Twitch, { GenericTwitchResponse, TwitchClip } from "twitch-api-helix";
 
 import Joi from "joi";
 
@@ -20,37 +20,61 @@ const schema = Joi.object({
 export async function getClips(params: any) {
   const { error, value } = schema.validate(params);
   if (error) {
-    throw new Error(error.message);
+    throw error;
   }
   const { type, name, first, start, end, after, before } = value;
+  let response: GenericTwitchResponse<TwitchClip>;
+
   switch (type) {
     case "game":
       const [game] = await twitchApi.getGameByName(name).catch((err) => {
-        throw new Error(`Game ${value.name} not found`);
+        const error = new Error(`Game ${value.name} not found`);
+        error.name = "GameNotFound";
+        throw error;
       });
 
-      return await twitchApi.getClips({
-        game_id: game.id,
-        first: first,
-        started_at: start,
-        ended_at: end,
-        after: after,
-        before: before,
-      });
+      response = await twitchApi
+        .getClips({
+          game_id: game.id,
+          first: first,
+          started_at: start,
+          ended_at: end,
+          after: after,
+          before: before,
+        })
+        .catch((err) => {
+          const error = new Error(`No clips found for game ${value.name}`);
+          error.name = "NoClipsFound";
+          throw error;
+        });
+      break;
     case "user":
       const [user] = await twitchApi.getUserByName(name).catch((err) => {
-        throw new Error(`User ${value.name} not found`);
+        const error = new Error(`User ${value.name} not found`);
+        error.name = "UserNotFound";
+        throw error;
       });
 
-      return await twitchApi.getClips({
-        broadcaster_id: user.id,
-        first: first,
-        started_at: start,
-        ended_at: end,
-        after: after,
-        before: before,
-      });
+      response = await twitchApi
+        .getClips({
+          broadcaster_id: user.id,
+          first: first,
+          started_at: start,
+          ended_at: end,
+          after: after,
+          before: before,
+        })
+        .catch((err) => {
+          const error = new Error(`No clips found for user ${value.name}`);
+          error.name = "NoClipsFound";
+          throw error;
+        });
+      break;
     default:
-      throw new Error("Invalid type");
+      const error = new Error("Invalid type");
+      error.name = "InvalidType";
+      throw error;
   }
+
+  return response;
 }
